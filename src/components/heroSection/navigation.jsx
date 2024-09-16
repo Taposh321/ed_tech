1a3a1134fe571b27e42998def41a9436649d1948
Taposh321 <taposhbarman422@gmail.com>
2024-09-16 12:13:51 +0600

    ui

--- src/components/heroSection/navigation.jsx
+++ src/components/heroSection/navigation.jsx
@@ -4,8 +4,9 @@ return(<>
     <div className="flex w-full p-4 items-center">
 
     <div className="logo max-w-[150px] w-full">
-        <img className="w-full h-full object-cover " src="./assets/logo.png" alt="Logo" srcset="" />
+        <img className="w-full h-full object-cover " src="./public/assets/logo.png" alt="Logo" srcset="" />
     </div>
+    <div className="flex flex-1 bg-green-500">
     <div className="navLinks flex justify-center flex-1 ">
            <ul className=" flex list-none  text-sm h-full p-0 m-0 gap-3">
             <li>Home</li>
@@ -20,6 +21,8 @@ return(<>
             <div className="rounded bg-[#2ea8d0] text-white px-4 py-1">Log in</div>
 
     </div>
+    </div>
+   
 
     </div>
 </>)
